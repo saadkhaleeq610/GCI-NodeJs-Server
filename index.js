4caa404c2daadb13e2c5854da7fdd559d742e4d4
saadkhaleeq610 <58370495+saadkhaleeq610@users.noreply.github.com>
2019-12-18 01:19:48 +0500

    GCI-SaadKhaleeq imported json file and updated the result.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+var packageFile = require('./package.json')
 
-const fs = require('fs');
-let rawdata = fs.readFileSync('package.json');
-const packageFile = JSON.parse(rawdata);
+//const fs = require('fs');
+//let rawdata = fs.readFileSync('package.json');
+//const packageFile = JSON.parse(rawdata);
 
 const app = express();
 const PORT = 8080;
 
 app.get('/info', (req, res) => {
-    res.json(packageFile);
-  });
+  let responseContent = { ServerName : packageFile.name,
+	Version : packageFile.version
+	} 
+  res.json(responseContent);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
